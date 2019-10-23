08fc4d9f294904529fa06d2afd3307022e919085
Ofir Arviv <t-ofarvi@microsoft.com>
2019-10-23 23:17:42 +0300

    add more features

--- tupa/alignment_feature_extractor.py
+++ tupa/alignment_feature_extractor.py
@@ -43,34 +43,69 @@ class AlignmentFeatureExtractor:
     def _get_features_params_dict():
         features_params = OrderedDict()
         projected_labels_param = FeatureParameters(suffix="l", dim=50, size=len(edge_tags)+1, dropout=0, updated=True,
-                                                   num=4, init=None, data=None, indexed=False, copy_from=None,
+                                                   num=12, init=None, data=None, indexed=False, copy_from=None,
                                                    filename=None, min_count=1, enabled=True, node_dropout=0, vocab=None,
                                                    lang_specific=False)
         features_params["l"] = projected_labels_param
 
-        projected_labels_param = NumericFeatureParameters(2)
+        projected_labels_param = NumericFeatureParameters(6)
         features_params["a_numeric"] = projected_labels_param
 
         return features_params
 
     def extract_features(self, state: State):
+        # self._get_features_from_template("b0_p1_l b0_n_d")
         s0 = AlignmentFeatureExtractor._get_node(state, 'stack', 0)
         s0_parent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, s0, 1)
         s0_grandparent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, s0, 2)
         b0 = AlignmentFeatureExtractor._get_node(state, 'buffer', 0)
         b0_parent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, b0, 1)
-        b0_grandparent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, b0, 1)
+        b0_grandparent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, b0, 2)
+
+        s1 = AlignmentFeatureExtractor._get_node(state, 'stack', 1)
+        s1_parent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, s1, 1)
+        s1_grandparent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, s1, 2)
+        b1 = AlignmentFeatureExtractor._get_node(state, 'buffer', 1)
+        b1_parent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, b1, 1)
+        b1_grandparent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, b1, 2)
+
+        s2 = AlignmentFeatureExtractor._get_node(state, 'stack', 2)
+        s2_parent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, s2, 1)
+        s2_grandparent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, s2, 2)
+        b2 = AlignmentFeatureExtractor._get_node(state, 'buffer', 2)
+        b2_parent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, b2, 1)
+        b2_grandparent_tag = AlignmentFeatureExtractor._extract_parent_tag(state, b2, 2)
 
         s0_depth = AlignmentFeatureExtractor._extract_depth(state, s0.orig_node) if s0 else 0
         b0_depth = AlignmentFeatureExtractor._extract_depth(state, b0.orig_node) if b0 else 0
+        s1_depth = AlignmentFeatureExtractor._extract_depth(state, s1.orig_node) if s1 else 0
+        b1_depth = AlignmentFeatureExtractor._extract_depth(state, b1.orig_node) if b1 else 0
+        s2_depth = AlignmentFeatureExtractor._extract_depth(state, s2.orig_node) if s2 else 0
+        b2_depth = AlignmentFeatureExtractor._extract_depth(state, b2.orig_node) if b2 else 0
 
         features = OrderedDict()
-        features['l'] = [s0_parent_tag, s0_grandparent_tag, b0_parent_tag, b0_grandparent_tag]
+        features['l'] = [s0_parent_tag, s0_grandparent_tag, b0_parent_tag, b0_grandparent_tag,
+                         s1_parent_tag, s1_grandparent_tag, b1_parent_tag, b1_grandparent_tag,
+                         s2_parent_tag, s2_grandparent_tag, b2_parent_tag, b2_grandparent_tag]
         assert self.features_params["l"].num == len(features['l'])
-        features['a_numeric'] = [s0_depth, b0_depth]
+        features['a_numeric'] = [s0_depth, b0_depth, s1_depth, b1_depth, s2_depth, b2_depth]
         assert self.features_params["a_numeric"].num == len(features['a_numeric'])
 
         return features
+
+    @staticmethod
+    def _get_features_from_template(template: str, state: State):
+        temp = [entry.split("_") for entry in template.split()]
+        for source, relation, feature in temp:
+            source_node = None
+            if source[0] == 'b':
+                source_node = AlignmentFeatureExtractor._get_node(state, 'buffer', source[1:])
+            elif source[0] == 's':
+                source_node = AlignmentFeatureExtractor._get_node(state, 'buffer', source[1:])
+            else:
+                raise Exception(f'Unknown source in template: {source}')
+
+            # if relation[0] == 'p'
 
     @staticmethod
     def _extract_depth(state: State, node: Node) -> int:
